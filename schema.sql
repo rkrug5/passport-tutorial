e6bc262bf4686ca6bd2c0ab5b2e2827c4880cdff
rkrug5 <phishermankrug@hotmail.com>
2018-05-11 06:17:09 -0700

    added a bunch

---
+++ schema.sql
@@ -0,0 +1,6 @@
+-- Drops the db if it exists currently --
+DROP DATABASE IF EXISTS sequelize_passport;
+-- Creates the "sequelize_passport" database --
+CREATE DATABASE sequelize_passport;
+
+USE sequelize_passport;
